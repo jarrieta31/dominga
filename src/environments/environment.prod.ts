324701ae014ffe6facd1ebc34b7e2a8849594fda
Julio Arrieta <jaservice@vera.com.uy>
2020-04-08 19:15:50 -0300

    Se agrega la carpeta functions utilizando el comando 'firebase init'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDhM4nqR6sQ9bDVZjc2zSSnRGoPKCJRFcM",
+    authDomain: "appdominga.firebaseapp.com",
+    databaseURL: "https://appdominga.firebaseio.com",
+    projectId: "appdominga",
+    storageBucket: "appdominga.appspot.com",
+    messagingSenderId: "99878739465",
+    appId: "1:99878739465:web:b831cd823949fc045be2d8",
+    measurementId: "G-BPNTCZ1PC9"
+  }
 };
