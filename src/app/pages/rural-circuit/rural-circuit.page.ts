8969d6d6f306665d328a04f32c062b7c96f1a600
SebaD95 <sebadiaz95@hotmail.com>
2020-04-11 19:40:16 -0300

    link desde mapa a lugar

--- src/app/pages/rural-circuit/rural-circuit.page.ts
+++ src/app/pages/rural-circuit/rural-circuit.page.ts
@@ -64,7 +64,7 @@ export class RuralCircuitPage implements OnInit {
                 this.items.forEach(data => {
                     if (data.tipo == 'Rural') {
                         var popup = new Mapboxgl.Popup({ offset: 25 }).setHTML(
-                            '<img src="'+data.url[0]+'" /><h5 style="text-align: center">'+data.nombre+'</h5>' 
+                            '<a href="http://localhost:8100/places/'+data.$key+'"><img src="'+data.url[0]+'" /><h5 style="text-align: center">'+data.nombre+'</h5></a>' 
                         );
                         const marker = new Mapboxgl.Marker({
                             draggable: false
