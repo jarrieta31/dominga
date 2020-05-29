d6e0e41a06955190104e26bfd33b48fe18588ed3
SebaD95 <sebadiaz95@hotmail.com>
2020-05-29 08:41:38 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -89,7 +89,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,7 @@ export const environment = {
     messagingSenderId: "99878739465",
     appId: "1:99878739465:web:b831cd823949fc045be2d8",
     measurementId: "G-BPNTCZ1PC9"
-  }
+  },
+  firebaseAPIKey: "AIzaSyDhM4nqR6sQ9bDVZjc2zSSnRGoPKCJRFcM",
+  mapBoxToken: "pk.eyJ1IjoiY2FzYWRvbWluZ2EiLCJhIjoiY2s3NTlzajFoMDVzZTNlcGduMWh0aml3aSJ9.JcZFoGdIQnz3hSg2p4FGkA"
 };
