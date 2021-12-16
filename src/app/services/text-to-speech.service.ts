69c408d1915331fa3a24e7752f38f1acc031001a
ramon <chedecime@gmail.com>
2021-12-15 21:23:21 -0300

    TextToSpeech: Refactorizacion: Se ajusta la velocidad.

--- src/app/services/text-to-speech.service.ts
+++ src/app/services/text-to-speech.service.ts
@@ -75,9 +75,9 @@ export class TextToSpeechService  {
 
   velocidad( v : string ){
     switch (v) {
-      case '1'  : return 1.2;
-      case '1.5': return 1.5;
-      case '2'  : return 2  ;
+      case '1'  : return 0.5;
+      case '1.5': return 1;
+      case '2'  : return 1.5  ;
         
       default: return 1;
     }
