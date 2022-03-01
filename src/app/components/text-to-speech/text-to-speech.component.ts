fd08a197bb82bed15e4f12a820d73b891f905687
Julio Arrieta <julioarrieta23@gmail.com>
2022-03-01 14:16:36 -0300

    Botton para leer en status bar
    - Se coloca el boton para leer en la barra status bar
      y solo se muestra un solo boton tipo fab-button

--- src/app/components/text-to-speech/text-to-speech.component.ts
+++ src/app/components/text-to-speech/text-to-speech.component.ts
@@ -1,43 +1,67 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 import { Platform } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-text-to-speech',
-  templateUrl: './text-to-speech.component.html',
-  styleUrls: ['./text-to-speech.component.scss'],
+    selector: 'app-text-to-speech',
+    templateUrl: './text-to-speech.component.html',
+    styleUrls: ['./text-to-speech.component.scss'],
 })
 
 export class TextToSpeechComponent implements OnInit {
 
-  @Input('texto') texto: string;
+    @Input('texto') texto: string;
+    viewImage = true;
+    rate: number = 1;
 
-  constructor(
-    private tts: TextToSpeech,
-    private platform: Platform,
-  ) { }
+    constructor(
+        private tts: TextToSpeech,
+        private platform: Platform,
+        private loading: LoadingController,
+    ) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        if (this.platform.is("ios") || this.platform.is("iphone")) {
+            this.rate = 1.4
+        }
+    }
 
-  }
+    ngOnDestroy(): void {
+        this.stopTextToSpeech()
+    }
 
-  playTextToSpeech() {
-    let rate: number;
-    if (this.platform.is("ios") || this.platform.is("iphone")) {
-      rate = 1.4
-    } else { rate = 1 };
-    console.log(this.texto)
-    this.tts.speak({ text: this.texto, locale: 'es-AR', rate: rate })
-      .then(() => console.log('Done'))
-      .catch((reason: any) =>
-        console.log(reason)
-      );
-  }
+    async presentLoading() {
+        const loading = await this.loading.create({
+            cssClass: 'my-custom-class',
+            message: 'Por favor aguarde, procesando texto',
+            duration: 2000
+        });
+        await loading.present();
 
-  stopTextToSpeech() {
-    this.tts.speak({ text: "", locale: 'es-AR', rate: 1 })
-      .then(() => console.log('Done'))
-      .catch((reason: any) => console.log(reason));
-  }
+        const { role, data } = await loading.onDidDismiss();
+        console.log('Loading dismissed!');
+    }
+
+    async playTextToSpeech() {
+        this.presentLoading();
+        this.viewImage = false;
+        let arrText = this.texto.split('.');
+        //console.log(arrText)
+
+        this.tts.speak({ text: this.texto, locale: 'es-AR', rate: this.rate })
+            .then(() => { this.stopTextToSpeech() })
+            .catch((reason: any) =>
+                // this.loading.dismiss()
+                console.log(reason)
+            );
+    }
+
+    stopTextToSpeech() {
+        //        this.loading.dismiss();
+        this.viewImage = true;
+        this.tts.speak({ text: "", locale: 'es-AR', rate: 1 })
+            .then(() => console.log('Done'))
+            .catch((reason: any) => console.log(reason));
+    }
 }
